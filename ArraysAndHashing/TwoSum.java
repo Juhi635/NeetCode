542efd2afc8c007fb3943d8421c8d935baf9a699
Juhi Dugar <132328037+Juhi635@users.noreply.github.com>
2025-12-26 19:08:27 +0530

    NeetCode Arrays & Hashing: Two Sum

---
+++ ArraysAndHashing/TwoSum.java
@@ -0,0 +1,22 @@
+// NeetCode Roadmap: Arrays & Hashing
+// https://leetcode.com/problems/two-sum/
+// Time: O(n), Space: O(n)
+
+import java.util.HashMap;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int needed = target - nums[i];
+
+            if (map.containsKey(needed)) {
+                return new int[]{map.get(needed), i};
+            }
+
+            map.put(nums[i], i);
+        }
+        return new int[]{};
+    }
+}
