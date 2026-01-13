9d61de0f1a143e54a105320c11e631cf7001d136
Juhi Dugar <132328037+Juhi635@users.noreply.github.com>
2026-01-13 17:44:19 +0530

    NeetCode Arrays & Hashing: Longest Consecutive Sequence

---
+++ ArraysAndHashing/LongestConsecutiveSequence.java
@@ -0,0 +1,33 @@
+// LeetCode 128: Longest Consecutive Sequence
+// NeetCode Roadmap: Arrays & Hashing
+// https://leetcode.com/problems/longest-consecutive-sequence/
+// Time: O(n), Space: O(n)
+
+import java.util.HashSet;
+
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            set.add(num);
+        }
+
+        int longest = 0;
+
+        for (int num : set) {
+            if (!set.contains(num - 1)) { // start of sequence
+                int currentNum = num;
+                int currentStreak = 1;
+
+                while (set.contains(currentNum + 1)) {
+                    currentNum++;
+                    currentStreak++;
+                }
+
+                longest = Math.max(longest, currentStreak);
+            }
+        }
+
+        return longest;
+    }
+}
