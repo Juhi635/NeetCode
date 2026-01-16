d83d285561bc1608e806cf4c01a5eba3bee96a6d
Juhi Dugar <132328037+Juhi635@users.noreply.github.com>
2026-01-16 19:23:50 +0530

    NeetCode Two Pointers: Valid Palindrome

---
+++ Two Pointers/ValidPalindrome.java
@@ -0,0 +1,29 @@
+// LeetCode 125: Valid Palindrome
+// NeetCode Roadmap: Two Pointers
+// https://leetcode.com/problems/valid-palindrome/
+// Time: O(n), Space: O(1)
+
+class Solution {
+    public boolean isPalindrome(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+            while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
+                left++;
+            }
+            while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
+                right--;
+            }
+
+            if (Character.toLowerCase(s.charAt(left)) !=
+                Character.toLowerCase(s.charAt(right))) {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
