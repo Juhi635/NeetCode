db95a3a2f2fb9d7dbe536606b6fdbafd1138c958
Juhi Dugar <132328037+Juhi635@users.noreply.github.com>
2026-01-18 16:12:09 +0530

    NeetCode Two Pointers: 3Sum

---
+++ Two Pointers/ThreeSum.java
@@ -0,0 +1,40 @@
+// LeetCode 15: 3Sum
+// NeetCode Roadmap: Two Pointers
+// https://leetcode.com/problems/3sum/
+// Time: O(n^2), Space: O(1) extra (excluding output)
+
+import java.util.*;
+
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+        int n = nums.length;
+
+        for (int i = 0; i < n - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+            int left = i + 1;
+            int right = n - 1;
+
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if (sum == 0) {
+                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    left++;
+                    right--;
+
+                    while (left < right && nums[left] == nums[left - 1]) left++;
+                    while (left < right && nums[right] == nums[right + 1]) right--;
+
+                } else if (sum < 0) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+        return result;
+    }
+}
